62763395fee60d4e00f68cdc0b02d82049aaa388
Hayato Ando <h.ando.creative.innovation@gmail.com>
2020-07-11 18:05:25 +0900

    add Chapter-6

---
+++ chapter-6.sh
@@ -0,0 +1,10 @@
+date
+
+cal
+
+# リダイレクション 出力内容 > 出力 先
+cal > calendar
+
+# (bc < question-chapter-6) > answer
+# bc 計算の結果を出力 qustion-chapter-6に入っている計算式をbcに渡し、それをanswerに出力
+bc < question-chapter-6 > answer
